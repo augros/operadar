a79c64c364467b5773e07c497af5ea2a670e2b9d
AUGROS Clotilde <clotilde.augros@meteo.fr>
2023-04-19 11:04:35 +0200

    change operad_conf_AROME_ICE3 with tmatrix and Arome path in /cnrm/precip

--- operad_conf.py
+++ operad_conf.py
@@ -9,11 +9,12 @@ Configuration file for operad.py
 """
 
 # ==========  Model simulation options ===============
-model='MesoNH'
+model='Arome'
 micro = "ICE3" # CLOE ICE3 / LIMA_SG / LIMA_AG / ICE4
 LIMToption="" #"" or "cstmu" the model variables are taken from LIMT simulation # but a constant mu is applied in the PSD  for the dpol variables calculation 
+CCIconst=800.
 list_types=['vv','cc','rr','ii','ss','gg']
-list_types_tot=['rr','ii','ss','gg','wg']
+list_types_tot = ['rr','ii','ss','gg','wg']
 
 MixedPhase="Fwposg" # 'Tpos' or 'Fwpos' or 'Fwposg' #
  
@@ -23,23 +24,30 @@ singletype=False #False #True # if True: computes dpol var for each type
 
 n_interpol = 32      # nb bornes to interpol (2**5: min et max pour LAM, ELEV, T, M, Fw)
 
+
 # ==========  Radar options =============== 
-band="C"
+band="S"
 distmax_rad = 255.*1000 #150*1000 # Distance max des données radar dont on calcule les pseudo-observations
 alt_max = 12000. # Altitude max des données radar utilisées pour le calcul des pseudo-observations
 radarloc="center" # radar location: center or latlon (if latlon ==> to be defined below buy user)
 #latrad=
 #lonrad=
 
-# ========== Directories / files name options =========
-timelist=[18] #range(1,36) #[5,6,7,8] #ech=[20] #[36]
+# ========= Zoom ==========================
+lat_min,lat_max=47.0,50.
+lon_min,lon_max=1.,4.
 
-pathmodel="/home/augros/DONNEES/MESONH/SUPERCELL/SIMU550/"+micro+'/'
-filestart="SU500.1.EXP01."
+
+# ========== Directories / files name options =========
+timelist=[8] #range(1,36) #[5,6,7,8] #ech=[20] #[36]
+
+#pathmodel="/home/augros/DONNEES/AROME/20220816/PEAROME/R09/"
+pathmodel="/cnrm/precip/users/augros/DONNEES/AROME/"
+filestart="historic.arome.franmg-01km30+00" #08:00.fa"
 
 # Tmatrix directory
 table_ind="" # number of the selected Tmatrix table 
-repTmat="." 
+repTmat="/cnrm/precip/users/augros/DONNEES/TMATRIX" #"." 
 
 # Output files
 pathfick=pathmodel+'k'+MixedPhase+'/'
@@ -48,8 +56,4 @@ pathTmat=repTmat+"/OUTPUT/"
 
 # ========== Constants =======================
 RT = 6371.229*10**3 # Earth radius
-
-
-
-         
 
--- operad_conf_AROME_ICE3.py
+++ operad_conf_AROME_ICE3.py
@@ -26,7 +26,7 @@ n_interpol = 32      # nb bornes to interpol (2**5: min et max pour LAM, ELEV, T
 
 
 # ==========  Radar options =============== 
-band="C"
+band="S"
 distmax_rad = 255.*1000 #150*1000 # Distance max des données radar dont on calcule les pseudo-observations
 alt_max = 12000. # Altitude max des données radar utilisées pour le calcul des pseudo-observations
 radarloc="center" # radar location: center or latlon (if latlon ==> to be defined below buy user)
@@ -41,13 +41,13 @@ lon_min,lon_max=1.,4.
 # ========== Directories / files name options =========
 timelist=[8] #range(1,36) #[5,6,7,8] #ech=[20] #[36]
 
-pathmodel="/home/augros/DONNEES/AROME/20220816/PEAROME/R09/"
-#pathmodel="/cnrm/precip/users/augros/DONNEES/AROME/"
+#pathmodel="/home/augros/DONNEES/AROME/20220816/PEAROME/R09/"
+pathmodel="/cnrm/precip/users/augros/DONNEES/AROME/"
 filestart="historic.arome.franmg-01km30+00" #08:00.fa"
 
 # Tmatrix directory
 table_ind="" # number of the selected Tmatrix table 
-repTmat="." 
+repTmat="/cnrm/precip/users/augros/DONNEES/TMATRIX" #"." 
 
 # Output files
 pathfick=pathmodel+'k'+MixedPhase+'/'
